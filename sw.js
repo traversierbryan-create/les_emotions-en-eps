b240fd95b70bb1a87eea95e45494bc508ced9eda
traversierbryan-create <traversierbryan@gmail.com>
2026-09-02 23:33:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="emotions-eps-v5";
+const CACHE="emotions-eps-v6";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
